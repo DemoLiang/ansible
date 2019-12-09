cb6d9349dddd34db39969f530628c844fefdad9f
Demo <vimliang@gmail.com>
2019-12-09 23:28:26 +0800

    add roles yml

--- setup.sh
+++ setup.sh
@@ -4,7 +4,19 @@ case "$1" in
     k8s)
         ansible-playbook -i hosts k8s.yml
         ;;
+    node)
+        ansible-playbook -i hosts node.yml
+        ;;
+    etcd)
+        ansible-playbook -i hosts etcd.yml
+        ;;
+    flannel)
+        ansible-playbook -i hosts flannel.yml
+        ;;
+    docker)
+        ansible-playbook -i hosts docker.yml
+        ;;
     *)
-        echo "Usage:$0 k8s"
+        echo "Usage:$0 k8s|node|etcd|flannel|docker"
         ;;
 esac
